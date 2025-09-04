9b519eb84e3141ef3e758cc3fb33c306f623a4ac
lorisj-elefant <loris@elefant.gg>
2025-09-04 08:52:48 -0700

    quick fix

--- src/autoclef/java/adris/altoclef/commands/BodyLanguageCommand.java
+++ src/autoclef/java/adris/altoclef/commands/BodyLanguageCommand.java
@@ -7,16 +7,20 @@ import adris.altoclef.commandsystem.Command;
 import adris.altoclef.commandsystem.CommandException;
 import adris.altoclef.tasks.movement.BodyLanguageTask;
 
-public class BodyLanguageCommand extends Command{
+public class BodyLanguageCommand extends Command {
     public BodyLanguageCommand() throws CommandException {
-        super("bodylang", "Perform some sort of dance/body language action. Action must be either `greeting`, `nod_head`, `shake_head`, `victory` Examples: `bodylang greeting` to do a greeting dance. `bodylang shake_head` shakes head, use when no other action, and saying no.", new Arg<>(String.class, "bodyLanguage"));
+        super("bodylang",
+                "Perform some sort of dance/body language action. Action must be either `greeting`, `nod_head`, `shake_head`, `victory` ",
+                new Arg<>(String.class, "bodyLanguage"));
     }
+
     @Override
     protected void call(AltoClefController mod, ArgParser parser) throws CommandException {
         String bodyLanguage = parser.get(String.class);
         mod.runUserTask(new BodyLanguageTask(bodyLanguage), () -> {
             System.out.println("Body language done");
-            this.finish();});
+            this.finish();
+        });
     }
 
 }
