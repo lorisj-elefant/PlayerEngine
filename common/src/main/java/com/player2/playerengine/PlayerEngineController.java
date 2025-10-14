cd135c9c06eabe92d0f3f3d5b412a89e040a3506
lorisj-elefant <loris@elefant.gg>
2025-10-14 14:19:14 -0700

    fix

--- common/src/main/java/com/player2/playerengine/PlayerEngineController.java
+++ common/src/main/java/com/player2/playerengine/PlayerEngineController.java
@@ -37,6 +37,7 @@ import com.player2.playerengine.automaton.api.IBaritone;
 import com.player2.playerengine.automaton.api.entity.LivingEntityInventory;
 import com.player2.playerengine.automaton.api.utils.IEntityContext;
 import com.player2.playerengine.automaton.api.utils.IInteractionController;
+import com.player2.playerengine.trackers.CacheTracker;
 
 import java.util.Arrays;
 import java.util.List;
@@ -71,6 +72,7 @@ public class PlayerEngineController {
    private EntityTracker entityTracker;
    private BlockScanner blockScanner;
    private SimpleChunkTracker chunkTracker;
+   private CacheTracker cacheTracker;
    private MiscBlockTracker miscBlockTracker;
    private CraftingRecipeTracker craftingRecipeTracker;
    private EntityStuckTracker entityStuckTracker;
@@ -113,6 +115,7 @@ public class PlayerEngineController {
       this.extraController = new PlayerExtraController(this);
       this.initializeBaritoneSettings();
       this.botBehaviour = new BotBehaviour(this);
+      this.cacheTracker = new CacheTracker(this);
       this.initializeCommands();
       PlayerEngineSettings.load(
             newSettings -> {
@@ -144,6 +147,7 @@ public class PlayerEngineController {
       this.trackerManager.tick();
       this.blockScanner.tick();
       this.taskRunner.tick();
+      this.cacheTracker.tick();
       this.inputControls.onTickPost();
       this.baritone.serverTick();
       this.player2apiService.trySendHeartbeat();
